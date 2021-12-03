f882e501cc54ff650c76315f5bc20b61dd6962ae
Paul Prince <paul.prince-ext@pernod-ricard.com>
2021-12-03 17:01:35 +0100

    clean day1

--- src/year_2021/day_1/first_challenge.py
+++ src/year_2021/day_1/first_challenge.py
@@ -1,9 +1,18 @@
+from typing import List
 from src.year_2021.day_1.input_values import input_values
 
-input_values_head = input_values[:-1]
-input_values_tail = input_values[1:]
 
-diff = [head - tail for head, tail in zip(input_values_head, input_values_tail)]
-filtered_diff = [1 for iteration in diff if iteration < 0]
+def solve_day_1_first_challenge() -> None:
+    diff_values = compute_diff_list(input_values)
+    filtered_diff = keep_one_from_list(diff_values)
+    print(f"solve_day_1_first_challenge solution is {len(filtered_diff)}")
 
-print(len(filtered_diff))
+
+def compute_diff_list(input_int_values: List[int]) -> List[int]:
+    input_values_head = input_int_values[:-1]
+    input_values_tail = input_int_values[1:]
+    return [head - tail for head, tail in zip(input_values_head, input_values_tail)]
+
+
+def keep_one_from_list(diff_values: List[int]) -> List[int]:
+    return [1 for iteration in diff_values if iteration < 0]
--- src/year_2021/day_1/second_challenge.py
+++ src/year_2021/day_1/second_challenge.py
@@ -1,20 +1,23 @@
+from typing import List
+
+from src.year_2021.day_1.first_challenge import compute_diff_list, keep_one_from_list
 from src.year_2021.day_1.input_values import input_values
 
-input_values_1 = input_values[:-2]
-input_values_2 = input_values[1:-1]
-input_values_3 = input_values[2:]
 
-window_values = diff = [
-    value_1 + value_2 + value_3
-    for value_1, value_2, value_3 in zip(input_values_1, input_values_2, input_values_3)
-]
+def solve_day_1_second_challenge() -> None:
+    window_values = compute_window_values(input_values)
+    window_diff_values = compute_diff_list(window_values)
+    filtered_window_diff = keep_one_from_list(window_diff_values)
+    print(f"solve_day_1_first_challenge solution is {len(filtered_window_diff)}")
 
-window_values_head = window_values[:-1]
-window_values_tail = window_values[1:]
 
-window_diff = [
-    head - tail for head, tail in zip(window_values_head, window_values_tail)
-]
-filtered_window_diff = [1 for iteration in window_diff if iteration < 0]
-
-print(len(filtered_window_diff))
+def compute_window_values(input_int_values: List[int]) -> List[int]:
+    input_values_1 = input_int_values[:-2]
+    input_values_2 = input_int_values[1:-1]
+    input_values_3 = input_int_values[2:]
+    return [
+        value_1 + value_2 + value_3
+        for value_1, value_2, value_3 in zip(
+            input_values_1, input_values_2, input_values_3
+        )
+    ]
