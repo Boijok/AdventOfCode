b18e91c5841677ec6a06a1fae5059b9930e51385
Paul Prince <paul.prince-ext@pernod-ricard.com>
2021-12-03 19:29:29 +0100

    cleaner code day 2

--- src/year_2021/day_2/first_challenge.py
+++ src/year_2021/day_2/first_challenge.py
@@ -1,28 +1,33 @@
+from pydantic import BaseModel
 from src.year_2021.day_2.input_values import input_list
 
-forward_list = [input_value for input_value in input_list if "forward" in input_value]
-down_list = [input_value for input_value in input_list if "down" in input_value]
-up_list = [input_value for input_value in input_list if "up" in input_value]
-print(f"forward_list is {forward_list}")
-print(f"down_list is {down_list}")
-print(f"up_list is {up_list}")
 
-forward_int_list = [
-    int(input_value.replace("forward ", "")) for input_value in forward_list
-]
-down_int_list = [int(input_value.replace("down ", "")) for input_value in down_list]
-up_int_list = [int(input_value.replace("up ", "")) for input_value in up_list]
-print(f"forward_int_list is {forward_int_list}")
-print(f"down_int_list is {down_int_list}")
-print(f"up_int_list is {up_int_list}")
+class Position(BaseModel):
+    forward: int = 0
+    depth: int = 0
 
-forward_sum = sum(forward_int_list)
-down_sum = sum(down_int_list)
-up_sum = sum(up_int_list)
-print(f"Forward by {forward_sum},Down by {down_sum},Up by {up_sum}")
+    def move_forward(self, value: str) -> None:
+        int_value = int(value.replace("forward ", ""))
+        self.forward = self.forward + int_value
 
-down_value = down_sum - up_sum
-print(f"down_value is {down_value}")
+    def move_down(self, value: str) -> None:
+        int_value = int(value.replace("down ", ""))
+        self.depth = self.depth + int_value
 
-final_value = forward_sum * down_value
-print(f"final_value is {final_value}")
+    def move_up(self, value: str) -> None:
+        int_value = int(value.replace("up ", ""))
+        self.depth = self.depth - int_value
+
+
+def solve_day_2_first_challenge() -> None:
+    position = Position()
+    for value in input_list:
+        if "forward" in value:
+            position.move_forward(value)
+        if "down" in value:
+            position.move_down(value)
+        if "up" in value:
+            position.move_up(value)
+
+    final_value = position.forward * position.depth
+    print(f"solve_day_2_first_challenge is {final_value}")
--- src/year_2021/day_2/second_challenge.py
+++ src/year_2021/day_2/second_challenge.py
@@ -1,24 +1,35 @@
+from pydantic import BaseModel
 from src.year_2021.day_2.input_values import input_list
 
-forward = 0
-depth = 0
-aim = 0
 
-for value in input_list:
-    print(f"Current value is {value}")
-    if "forward" in value:
+class Position(BaseModel):
+    forward: int = 0
+    depth: int = 0
+    aim: int = 0
+
+    def move_forward(self, value: str) -> None:
         int_value = int(value.replace("forward ", ""))
-        forward = forward + int_value
-        aim_step = aim * int_value
-        print(f"aim_step is {aim_step}")
-        depth = depth + aim_step
-    if "down" in value:
-        int_value = int(value.replace("down ", ""))
-        aim = aim + int_value
-    if "up" in value:
-        int_value = int(value.replace("up ", ""))
-        aim = aim - int_value
-    print(f"new position is forward by {forward},depth by {depth},aim by {aim}")
+        self.forward = self.forward + int_value
+        self.depth = self.depth + self.aim * int_value
 
-final_value = forward * depth
-print(f"final_value is {final_value}")
+    def move_down(self, value: str) -> None:
+        int_value = int(value.replace("down ", ""))
+        self.aim = self.aim + int_value
+
+    def move_up(self, value: str) -> None:
+        int_value = int(value.replace("up ", ""))
+        self.aim = self.aim - int_value
+
+
+def solve_day_2_second_challenge() -> None:
+    position = Position()
+    for value in input_list:
+        if "forward" in value:
+            position.move_forward(value)
+        if "down" in value:
+            position.move_down(value)
+        if "up" in value:
+            position.move_up(value)
+
+    final_value = position.forward * position.depth
+    print(f"solve_day_2_second_challenge is {final_value}")
